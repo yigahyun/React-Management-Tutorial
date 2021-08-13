0be1d21a419316664e9ade5e30786e294c43729e
이가현 <yigahyun96@gmail.com>
2021-08-13 14:53:05 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "gray-background">
+        <img src = {logo} lat="logo"/>
+        <h2>Let's develop management system!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
